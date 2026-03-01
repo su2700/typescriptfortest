4bffd4ed5aede2bc1fa02678cc4dee62b38e4a05
su2700 <lhllfc@gmail.com>
2026-03-01 20:38:25 +0100

    feat: define and demonstrate a `CustomArray` type alias for mixed string and number arrays.

--- array.ts
+++ array.ts
@@ -4,8 +4,17 @@ console.log(num)
 let num2: Array<number> = [1,2,3,4,5]
 console.log(num2)
 
+//array of string
 let str: string[] = ["a","b","c","d","e"]
 console.log(str)
 
+//array of mixed type
 let arr: (string | number)[] = [1,"a",2,"b",3,"c"]
 console.log(arr)
+
+// 
+type CustomArray = (string | number)[]
+let arr2: CustomArray = [1,"a",2,"b",3,"c"]
+console.log(arr2)
+let arr3: CustomArray = [1,"d",4,"e",5,"f"]
+console.log(arr3)
