349ec5efc1b009b303ea65b129aae6a22c8ed0d9
su2700 <lhllfc@gmail.com>
2026-03-01 20:26:18 +0100

    feat: Add examples for `Array<number>` and union type array declarations.

--- array.ts
+++ array.ts
@@ -1,5 +1,11 @@
 let num: number[] = [1,2,3,4,5]
 console.log(num)
 
+let num2: Array<number> = [1,2,3,4,5]
+console.log(num2)
+
 let str: string[] = ["a","b","c","d","e"]
 console.log(str)
+
+let arr: (string | number)[] = [1,"a",2,"b",3,"c"]
+console.log(arr)
